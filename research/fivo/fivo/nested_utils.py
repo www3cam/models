00818ea76628a47a51ea54e6dbae80c9ef1f2ce1
www3cam <cameronfen@gmail.com>
2020-08-21 15:53:58 -0400

    Update nested_utils.py

--- research/fivo/fivo/nested_utils.py
+++ research/fivo/fivo/nested_utils.py
@@ -20,7 +20,7 @@ from __future__ import division
 from __future__ import print_function
 
 import itertools
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 
 from tensorflow.python.util import nest
 
